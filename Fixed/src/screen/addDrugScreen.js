614cadbedacd14913b9f6e7ed635db7b9a3e663e
dongyan <7632632@qq.com>
2018-05-17 00:38:41 +0800

    完善添加药物界面UI

--- Fixed/src/screen/addDrugScreen.js
+++ Fixed/src/screen/addDrugScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import {ScrollView, Button, View} from 'react-native';
+import {ScrollView, Button, View, Text, StyleSheet} from 'react-native';
 import DrugService from "../service/drugService";
-import {ListRow, Input, Toast, Checkbox, Label} from "teaset";
+import {ListRow, Input, Toast, Checkbox, Label, Badge} from "teaset";
+import {RkCard, RkStyleSheet} from "react-native-ui-kitten";
+import {
+    RkSwitch
+} from '../component';
 
-const KEY = "Drugs";
 /**
  *
  */
@@ -45,126 +48,209 @@ export default class AddDrugScreen extends React.Component {
 
     render() {
         return (
-            <ScrollView style={{flex: 1}}>
-                <ListRow detail={
-                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                        <Input
-                            style={{flex: 4}}
-                            size='md'
-                            onChangeText={(text) => this.setState({drugName: text})}
-                            value={this.state.text}
-                            placeholder={"药品名称"}
-                        />
-                        <Label
-                            style={{flex: 2, paddingLeft: 2, paddingRight: 2}}
-                            size="md"
-                            text=''/>
-                        <Input
-                            style={{flex: 2}}
-                            size='md'
-                            onChangeText={(text) => this.setState({props: {count: text}})}
-                            value={this.state.props.count}
-                            placeholder={"20"}
-                        />
-                        <Label
-                            style={{paddingLeft: 2, paddingRight: 2}}
-                            size="md"
-                            text='X'/>
-                        <Input
-                            style={{flex: 2}}
-                            size='md'
-                            onChangeText={(text) => this.setState({props: {unit: text}})}
-                            value={this.state.props.unit}
-                            placeholder={"0.5mg"}
-                        />
+            <ScrollView style={styles.container}>
+                <RkCard style={styles.card}>
+                    <View rkCardHeader>
+                        <Text>药品信息</Text>
                     </View>
-                }/>
-                <ListRow detail={
-                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                        <Checkbox
-                            title='早晨'
-                            style={{flex: 1}}
-                            checked={this.state.isMorning}
-                            onChange={checked => this.setState({isMorning: checked})}
-                        />
-                        <Input
-                            style={{flex: 2}}
-                            size='md'
-                            onChangeText={(text) => this.setState({dosage: text})}
-                            value={this.state.text}
-                            placeholder={"每日用量"}
-                        />
-                        <Label
-                            style={{paddingLeft: 1, paddingRight: 2}}
-                            size="md"
-                            text='X'/>
-                        <Input
-                            style={{flex: 1}}
-                            size='md'
-                            onChangeText={(text) => this.setState({dosage: text})}
-                            value={this.state.props.unit}
-                            disabled={true}
-                            placeholder={"0.5mg"}
-                        />
+                    <View rkCardContent>
+                        <ScrollView style={{flex: 1}}>
+                            <ListRow detail={
+                                <View style={styles.row}>
+                                    <Input
+                                        style={{flex: 5}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({drugName: text})}
+                                        value={this.state.text}
+                                        placeholder={"药品名称"}
+                                    />
+                                    <Label
+                                        style={{flex: 1, paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text=''/>
+                                    <Input
+                                        style={{flex: 2}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({props: {count: text}})}
+                                        value={this.state.props.count}
+                                        placeholder={"20"}
+                                    />
+                                    <Label
+                                        style={{paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text='*'/>
+                                    <Input
+                                        style={{flex: 2}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({props: {unit: text}})}
+                                        value={this.state.props.unit}
+                                        placeholder={"0.5mg"}
+                                    />
+                                </View>}/>
+                            <ListRow detail={
+                                <View style={styles.row}>
+                                    <Input
+                                        style={{flex: 5}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({drugName: text})}
+                                        value={this.state.text}
+                                        placeholder={"药品名称"}
+                                    />
+                                    <Label
+                                        style={{flex: 1, paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text=''/>
+                                    <Input
+                                        style={{flex: 2}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({props: {count: text}})}
+                                        value={this.state.props.count}
+                                        placeholder={"20"}
+                                    />
+                                    <Label
+                                        style={{paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text='*'/>
+                                    <Input
+                                        style={{flex: 2}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({props: {unit: text}})}
+                                        value={this.state.props.unit}
+                                        placeholder={"0.5mg"}
+                                    />
+                                </View>}/>
+                        </ScrollView>
                     </View>
-                }/>
-                <ListRow detail={
-                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                        <Checkbox
-                            title='早晨'
-                            style={{flex: 1}}
-                            checked={this.state.isMorning}
-                            onChange={checked => this.setState({isMorning: checked})}
-                        />
-                        <Input
-                            style={{flex: 2}}
-                            size='md'
-                            onChangeText={(text) => this.setState({dosage: text})}
-                            value={this.state.text}
-                            placeholder={"每日用量"}
-                        />
-                        <Label
-                            style={{paddingLeft: 1, paddingRight: 2}}
-                            size="md"
-                            text='X'/>
-                        <Input
-                            style={{flex: 1}}
-                            size='md'
-                            onChangeText={(text) => this.setState({dosage: text})}
-                            value={this.state.props.unit}
-                            placeholder={"0.5mg"}
-                        />
+
+                </RkCard>
+                <RkCard style={styles.card}>
+                    <View rkCardHeader>
+                        <Text>用药计划</Text>
                     </View>
-                }/>
-                <ListRow detail={
-                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                        <Checkbox
-                            title='早晨'
-                            style={{flex: 1}}
-                            checked={this.state.isMorning}
-                            onChange={checked => this.setState({isMorning: checked})}
-                        />
-                        <Input
-                            style={{flex: 2}}
-                            size='md'
-                            onChangeText={(text) => this.setState({dosage: text})}
-                            value={this.state.text}
-                            placeholder={"每日用量"}
-                        />
-                        <Label
-                            style={{paddingLeft: 1, paddingRight: 2}}
-                            size="md"
-                            text='X'/>
-                        <Input
-                            style={{flex: 1}}
-                            size='md'
-                            onChangeText={(text) => this.setState({dosage: text})}
-                            value={this.state.props.unit}
-                            placeholder={"0.5mg"}
-                        />
+                    <View rkCardContent>
+                        <ScrollView style={{flex: 1}}>
+                            <ListRow icon={require('../asserts/images/morning.png')} detail={
+                                <View style={styles.row}>
+                                    <Input
+                                        style={{flex: 1}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({dosage: text})}
+                                        value={this.state.text}
+                                        placeholder={"每日用量"}
+                                    />
+                                    <Label
+                                        style={{paddingLeft: 1, paddingRight: 2}}
+                                        size="sm"
+                                        text='*'/>
+                                    <Input
+                                        style={{flex: 1}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({dosage: text})}
+                                        value={this.state.props.unit}
+                                        placeholder={"0.5mg"}
+                                    />
+                                    <Label
+                                        style={{flex: 1, paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text=''/>
+                                    <RkSwitch style={styles.switch}
+                                              value={this.state.sendPush}
+                                              name="Push"
+                                              onValueChange={(sendPush) => this.setState({sendPush})}/>
+                                </View>
+                            }/>
+                            <ListRow icon={require('../asserts/images/noon.png')} detail={
+                                <View style={styles.row}>
+                                    <Input
+                                        style={{flex: 1}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({dosage: text})}
+                                        value={this.state.text}
+                                        placeholder={"每日用量"}
+                                    />
+                                    <Label
+                                        style={{paddingLeft: 1, paddingRight: 2}}
+                                        size="sm"
+                                        text='*'/>
+                                    <Input
+                                        style={{flex: 1}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({dosage: text})}
+                                        value={this.state.props.unit}
+                                        placeholder={"0.5mg"}
+                                    />
+                                    <Label
+                                        style={{flex: 1, paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text=''/>
+                                    <RkSwitch style={styles.switch}
+                                              value={this.state.sendPush}
+                                              name="Push"
+                                              onValueChange={(sendPush) => this.setState({sendPush})}/>
+                                </View>
+                            }/>
+                            <ListRow icon={require('../asserts/images/night.png')} detail={
+                                <View style={styles.row}>
+                                    <Input
+                                        style={{flex: 1}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({dosage: text})}
+                                        value={this.state.text}
+                                        placeholder={"每日用量"}
+                                    />
+                                    <Label
+                                        style={{paddingLeft: 1, paddingRight: 2}}
+                                        size="sm"
+                                        text='*'/>
+                                    <Input
+                                        style={{flex: 1}}
+                                        size='sm'
+                                        onChangeText={(text) => this.setState({dosage: text})}
+                                        value={this.state.props.unit}
+                                        placeholder={"0.5mg"}
+                                    />
+                                    <Label
+                                        style={{flex: 1, paddingLeft: 2, paddingRight: 2}}
+                                        size="sm"
+                                        text=''/>
+                                    <RkSwitch style={styles.switch}
+                                              value={this.state.sendPush}
+                                              name="Push"
+                                              onValueChange={(sendPush) => this.setState({sendPush})}/>
+                                </View>
+                            }/>
+                        </ScrollView>
                     </View>
-                }/>
+
+                </RkCard>
+
             </ScrollView>
         );
     }
 }
+
+let styles = RkStyleSheet.create(theme => ({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.screen.scroll,
+        paddingVertical: 8,
+        paddingHorizontal: 14
+    },
+    card: {
+        marginVertical: 8,
+    },
+    section: {
+        marginVertical: 25
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // paddingHorizontal: 17.5,
+        // borderBottomWidth: StyleSheet.hairlineWidth,
+        borderColor: theme.colors.border.base,
+        alignItems: 'center'
+    },
+    switch: {
+        marginVertical: 14
+    },
+}));
